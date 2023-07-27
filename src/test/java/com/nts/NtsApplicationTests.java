a44b4cb246164c0e7c9699f6355b087227ec36f9
inkyu-yoon <tryingmybest24h@gmail.com>
2023-07-27 12:56:50 +0900

    [build] : 프로젝트 생성

---
+++ src/test/java/com/nts/NtsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NtsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
