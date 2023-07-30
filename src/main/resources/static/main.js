a54df54c0fad65472c5a383f6a15793b70e82648
inkyu-yoon <tryingmybest24h@gmail.com>
2023-07-30 14:23:04 +0900

    [feat] : 게시글 등록 버튼 추가
    - 사용자명, 비밀번호, 제목, 내용, 해시태그를 입력받는다.

--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -39,3 +39,80 @@ function changePageSize(size) {
     // 원하는 URL로 이동
     window.location.href = "?size="+size;
 }
+
+
+// 게시글 등록 모달
+function openPostModal() {
+    const postModal = document.getElementById('postModal');
+    postModal.style.display = 'block';
+}
+
+// 게시글 등록 API 요청 '/api/v1/posts' POST 요청
+function registerPost() {
+    const username = document.getElementById('postUsername').value;
+    const password = document.getElementById('postUserPassword').value;
+    const postTitle = document.getElementById('postTitle').value;
+    const postBody = document.getElementById('postBody').value;
+    const hashtags = [];
+
+
+    const hashtagInputs = document.querySelectorAll('#hashtags input');
+
+    hashtagInputs.forEach(input => {
+        if (input.value.trim() !== '') {
+            hashtags.push(input.value.trim());
+        }
+    });
+
+    const postData = {
+        name: username,
+        password: password,
+        title: postTitle,
+        body: postBody,
+        hashtags: hashtags.slice(0, 5) // 최대 5개만 추출
+    };
+
+    // POST 요청 보내기
+    axios.post('/api/v1/posts', postData)
+        .then(function (response) {
+            // 등록 성공 시, 모달 닫고 게시글 목록 갱신
+            location.reload();
+        })
+        .catch(function (error) {
+            console.log(error);
+            alert(error.response.data.result);
+        });
+}
+
+// 게시글 등록 모달 닫기
+function closeAndClearPostModal() {
+    const postModal = document.getElementById('postModal');
+    postModal.style.display = 'none';
+
+    document.getElementById('postUsername').value = '';
+    document.getElementById('postUserPassword').value = '';
+    document.getElementById('postTitle').value = '';
+    document.getElementById('postBody').value = '';
+
+    // 해시태그 입력 란 초기화
+    const hashtagsDiv = document.getElementById('hashtags');
+    hashtagsDiv.innerHTML = `
+            <input type="text" class="form-control" placeholder="해시태그 입력">`;
+    hashtagCount = 1;
+}
+
+
+let hashtagCount = 1;
+
+// 해시태그 입력 란 추가하는 함수
+function addHashtagInput() {
+    if (hashtagCount >= 5) return; // 최대 5개까지만 추가
+
+    const hashtagsDiv = document.getElementById('hashtags');
+    const newInput = document.createElement('input');
+    newInput.type = 'text';
+    newInput.className = 'form-control mt-1';
+    newInput.placeholder = '해시태그 입력';
+    hashtagsDiv.appendChild(newInput);
+    hashtagCount++;
+}
